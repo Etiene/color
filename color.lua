2f98a5d85fe47423706e47866b881a7c561817c7
Etiene Dalcol <Etiene@users.noreply.github.com>
2017-08-05 14:18:47 +0200

    Initial commit with hex to grb conversion

---
+++ color.lua
@@ -0,0 +1,24 @@
+local M = {
+    colors = {
+        fucsia = 0xFF00FF,
+        blue = 0x0000FF,
+        red = 0xFF0000,
+        green = 0x00FF00,
+        white = 0xFFFFFF,
+        off = 0
+    }
+}
+
+local function hex_to_rgb(hex_color)
+        return {r = hex_color/(2^16) % 0x100,
+                g = hex_color/(2^8) % 0x100,
+                b = hex_color % 0x100
+                }
+end
+
+function M.hex(hex_color)
+    local c = hex_to_rgb(hex_color)
+    return string.char(c.g, c.r, c.b)
+end
+
+return M
